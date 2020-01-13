df13905c9620cee7de9bda09981118cd3916b3fc
Pantilei <ianulov.pantilei@gmail.com>
2020-01-14 00:47:47 +0200

    Updates

---
+++ precache-manifest.d90b4c1bfce06a8a61712f3b28c87a08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c226f0a7c9ff530a5b07c66975c6c6b",
+    "url": "/business/index.html"
+  },
+  {
+    "revision": "8f4150267d4f733b377e",
+    "url": "/business/static/css/main.d17b337d.chunk.css"
+  },
+  {
+    "revision": "c40b032f76d3c7247f2c",
+    "url": "/business/static/js/2.c75b4a05.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/business/static/js/2.c75b4a05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f4150267d4f733b377e",
+    "url": "/business/static/js/main.13067d55.chunk.js"
+  },
+  {
+    "revision": "f07ffdfabda1a69f560e",
+    "url": "/business/static/js/runtime-main.6aced53d.js"
+  },
+  {
+    "revision": "2928669e4e1053a413baf46bd30887e6",
+    "url": "/business/static/media/logo.2928669e.JPG"
+  },
+  {
+    "revision": "365328a923460675f19c76c9158ad31f",
+    "url": "/business/static/media/logoFooter.365328a9.JPG"
+  }
+]);
